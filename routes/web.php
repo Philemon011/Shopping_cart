1ef0e9009b34a3525d0bc876335c2a38e3a42efb
Philemon <etoundelight1@gmail.com>
2024-03-14 10:01:17 +0100

    editing index

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\ClientController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+
+ Route::get('/', [ClientController::class, 'home']);
